707820a8499bc5bd4a1f215ba8e6a2d5311cfd93
kenjikarasawa <kenji_karasawa@v-kki.co.jp>
2022-08-26 17:10:17 +0900

    unified_idの追加
    プロダクトの詳細に項目の追加

--- kki_products/models/product.py
+++ kki_products/models/product.py
@@ -9,6 +9,8 @@ class kki_products(models.Model):
     name2 = fields.Char()
     marge_name = fields.Char(compute="_get_marge_name", store=True)
 
+    unified_id = fields.Char()
+
     @api.depends('name', 'name2')
     def _get_marge_name(self):
         for record in self:
